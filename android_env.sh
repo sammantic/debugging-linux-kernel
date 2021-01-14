6ef7b05c6ece764a7476bb285fc84d0b0a5ef4d8
ALSAMMAN Mohamed <mohamed.alsamman@inria.fr>
2021-01-14 12:51:04 +0100

    Add AOSP config

---
+++ android_env.sh
@@ -0,0 +1,5 @@
+export CC=$(pwd)/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=$(pwd)/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export ARCH=arm64
+export SUBARCH=arm64
