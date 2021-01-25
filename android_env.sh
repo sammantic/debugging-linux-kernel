e0ebc5e3dc4a1bd5188c0c37780a09dd1cf5e9fe
M.Samman <sammantic4@gmail.com>
2021-01-25 02:07:22 +0100

    Update android_env.sh

--- android_env.sh
+++ android_env.sh
@@ -1,3 +1,4 @@
+#AOSP cross compiles exist under /prebuilts/gcc
 export CC=$(pwd)/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE=$(pwd)/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
